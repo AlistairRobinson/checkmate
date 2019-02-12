90bb7825dacd469995c42f90922ecbc54213013e
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-02-12 21:50:38 +0000

    Changed to use MongoDB

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, abort
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.checkmate
+
+@app.route('/retrieve', methods = ['POST'])
+def retrieve():
+    if not request.json or not 'key' in request.json or not 'email' in request.json:
+        abort(400)
+    else:
+        abort(400)
+
+@app.route('/register', methods = ['POST'])
+def retrieve():
+    if not request.json or not 'key' in request.json or not 'email' in request.json:
+        abort(400)
+    else:
+        abort(400)
