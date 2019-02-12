0360865bb442c83f56ca2c5b2717497869e7c533
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-02-12 22:55:33 +0000

    Added word and PIN generation

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from flask import Flask, request, abort, jsonify
 from pymongo import MongoClient
 from datetime import datetime
+import random
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.checkmate
+
+with open('dict/nouns.txt') as f:
+    nouns = [l.rstrip('\n') for l in f]
+
+with open('dict/verbs.txt') as f:
+    verbs = [l.rstrip('\n') for l in f]
+
+with open('dict/adjectives.txt') as f:
+    adjectives = [l.rstrip('\n') for l in f]
+
+with open('dict/adverbs.txt') as f:
+    adverbs = [l.rstrip('\n') for l in f]
+
+def generate_pin():
+    random.seed(datetime.now())
+    return ''.join(map(str, random.sample(range(10), 4)))
+
+def generate_words():
+    random.seed(datetime.now())
+    case_of = {
+        1: [random.choice(nouns), random.choice(verbs), random.choice(nouns)],
+        2: [random.choice(nouns), random.choice(verbs), random.choice(adverbs)],
+        3: [random.choice(adjectives), random.choice(nouns), random.choice(verbs)],
+        4: [random.choice(verbs), random.choice(nouns), random.choice(adverbs)],
+        5: [random.choice(adverbs), random.choice(verbs), random.choice(nouns)],
+        6: [random.choice(adjectives), random.choice(adjectives), random.choice(nouns)],
+        7: [random.choice(adverbs), random.choice(nouns), random.choice(verbs)]
+    }
+    return case_of.get(random.randint(1, 7))
+
+print(generate_pin())
+print(generate_words())
 
 @app.route('/retrieve', methods = ['POST'])
 def retrieve():
