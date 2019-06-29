e94e73ed4f149c803d2a17dcb8c934870f3f0277
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-06-29 16:21:12 +0100

    Add JS for typewriter

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    typewriter();
 });
+
+function typewriter() {
+    var showcase = document.getElementById('showcase');
+    var typewriter = new Typewriter(showcase, { 
+        loop: true,
+        cursor: "_"
+    });
+    console.log("hello");
+    typewriter.typeString('Hello World!')
+        .pauseFor(2500)
+        .deleteAll()
+        .pauseFor(2500)
+        .start();
+}
