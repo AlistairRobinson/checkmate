08ce1c51dd5a2d3380d6331ced43ccda08b81bd6
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-06-29 16:44:16 +0100

    Enforce higher security measures in responses

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,14 @@ def create_app(test_config=None):
 
     from . import api
     app.register_blueprint(api.bp)
+
+    @app.after_request
+    def after_request(response):
+        csp = "default-src 'self' 'unsafe-inline' https://*.googleapis.com https://cdnjs.cloudflare.com https://use.fontawesome.com"
+        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        response.headers['Content-Security-Policy'] = csp
+        return response
     
     @app.route('/', methods = ['GET'])
     def index():
