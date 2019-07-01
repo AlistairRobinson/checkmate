3514a6520f3bbcce59cc29c024e7830bea498086
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-01 18:26:37 +0100

    Add Google recaptcha support

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,11 @@ def create_app(test_config=None):
     from . import stats
     app.jinja_env.globals['apis'] = stats.apis()
     app.jinja_env.globals['accounts'] = stats.accounts()
+    app.jinja_env.globals['sitekey'] = stats.sitekey()
 
     @app.after_request
     def after_request(response):
-        csp = "default-src 'self' 'unsafe-inline' https://*.googleapis.com https://cdnjs.cloudflare.com https://use.fontawesome.com"
+        csp = "default-src 'self' 'unsafe-inline' https://*.googleapis.com https://*.google.com https://*.gstatic.com https://cdnjs.cloudflare.com https://use.fontawesome.com"
         response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
         response.headers['X-Frame-Options'] = 'SAMEORIGIN'
         response.headers['Content-Security-Policy'] = csp
--- app/stats.py
+++ app/stats.py
@@ -4,6 +4,10 @@ with open('secret/dbpass.txt') as f:
     dbpass = [l.rstrip('\n') for l in f]
     dbpass = dbpass[0]
 
+with open('secret/sitekey.txt') as f:
+    sitekeypass = [l.rstrip('\n') for l in f]
+    sitekeypass = sitekeypass[0]
+
 client = MongoClient('mongodb+srv://alistair:' + dbpass + '@checkmate-kyna9.azure.mongodb.net/test?retryWrites=true&w=majority')
 db = client.Checkmate
 
@@ -12,3 +16,6 @@ def apis():
 
 def accounts():
     return db.registry.count()
+
+def sitekey():
+    return sitekeypass
