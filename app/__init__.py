0cde12e8d83ff7dc0b34a148a97b298786d4be6a
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-06 23:19:52 +0100

    Restructure __init__.py to work with AWS

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, abort, jsonify, Blueprint, render_template
 
-def create_app(test_config=None):
+application = Flask(__name__)
+app = application
 
-    app = Flask(__name__)
+from . import api
+app.register_blueprint(api.bp)
 
-    from . import api
-    app.register_blueprint(api.bp)
+from . import site
+app.register_blueprint(site.bp)
 
-    from . import site
-    app.register_blueprint(site.bp)
+from . import stats
+app.jinja_env.globals['apis'] = stats.apis
+app.jinja_env.globals['accounts'] = stats.accounts
 
-    from . import stats
-    app.jinja_env.globals['apis'] = stats.apis
-    app.jinja_env.globals['accounts'] = stats.accounts
-
-    @app.after_request
-    def after_request(response):
-        csp = "default-src 'self' 'unsafe-inline' https://*.googleapis.com https://*.google.com https://*.gstatic.com https://cdnjs.cloudflare.com https://use.fontawesome.com"
-        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-        response.headers['Content-Security-Policy'] = csp
-        return response
+@app.after_request
+def after_request(response):
+    csp = "default-src 'self' 'unsafe-inline' https://*.googleapis.com https://*.google.com https://*.gstatic.com https://cdnjs.cloudflare.com https://use.fontawesome.com"
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['Content-Security-Policy'] = csp
+    return response
     
-    return app
+if __name__ == "__main__":         
+    app.run()
