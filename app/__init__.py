1552a1fdac1adc3bb16deda67c72fbf4d662f535
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-07 11:02:41 +0100

    Add secret key for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, abort, jsonify, Blueprint, render_template
 
+with open('secret/secretkey.txt') as f:
+    secretkey = [l.rstrip('\n') for l in f]
+    secretkey = secretkey[0]
+
 application = Flask(__name__)
 app = application
+
+app.config.update(
+    TESTING=False,
+    SECRET_KEY=secretkey
+)
 
 from app import api
 app.register_blueprint(api.bp)
