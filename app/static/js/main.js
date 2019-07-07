e5f96aed38a102175f2a77f6fa997442dbeb957c
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-07-07 11:32:58 +0100

    Add hamburger menu to all webpages

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,36 +3,4 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
-
-    typewriter();
 });
-
-function typewriter() {
-    var showcase = document.getElementById('showcase');
-    var typewriter = new Typewriter(showcase, { 
-        loop: false,
-        cursor: "_"
-    });
-    (function execute() { 
-        typewriter.deleteAll()
-        .pauseFor(1000)
-        .typeString(getCode())
-        .pauseFor(1000)
-        .start();
-        setTimeout(execute, 10000);
-    })();
-}
-
-function getCode() {
-    var example = "";
-    $.post({
-        url: "/showcase", 
-        datatype: "json",
-        async: false,
-        success: function(result) {
-            example = result['w1'] + " " + result['w2'] + " "
-                    + result['w3'] + " " + result['pin'];
-        }
-    });
-    return example;
-}
---
+++ app/static/js/type.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    typewriter();
+});
+
+function typewriter() {
+    var showcase = document.getElementById('showcase');
+    var typewriter = new Typewriter(showcase, { 
+        loop: false,
+        cursor: "_"
+    });
+    (function execute() { 
+        typewriter.deleteAll()
+        .pauseFor(1000)
+        .typeString(getCode())
+        .pauseFor(1000)
+        .start();
+        setTimeout(execute, 10000);
+    })();
+}
+
+function getCode() {
+    var example = "";
+    $.post({
+        url: "/showcase", 
+        datatype: "json",
+        async: false,
+        success: function(result) {
+            example = result['w1'] + " " + result['w2'] + " "
+                    + result['w3'] + " " + result['pin'];
+        }
+    });
+    return example;
+}
